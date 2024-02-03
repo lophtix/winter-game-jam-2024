d2408b7751b277740fcbb92d0caedaf7c7fc8ad3
Benjamin Sundvall <benjamin@sundvall.org>
2024-02-03 19:01:22 +0100

    More boxes and aspect ratio

--- godot/Scripts/Game3D.gd
+++ godot/Scripts/Game3D.gd
@@ -11,16 +11,16 @@ class_name Game3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+    pass
 
 func add_flying_shoe(flying_shoe : FlyingShoe, from : Vector2, to : Vector2, speed):
-	var from_3d = camera.project_position(from, camera.near + offset_spawn)
-	var to_3d = camera.project_position(to, abs(camera.position.z))
-	
-	flying_shoe.set_movement(from_3d, to_3d, speed)
-	
-	shoe_container.add_child(flying_shoe)
+    var from_3d = camera.project_position(from, camera.near + offset_spawn)
+    var to_3d = camera.project_position(to, abs(camera.position.z))
+    
+    flying_shoe.set_movement(from_3d, to_3d, speed)
+    
+    shoe_container.add_child(flying_shoe)
--- godot/Scripts/GameBox.gd
+++ godot/Scripts/GameBox.gd
@@ -16,69 +16,69 @@ class_name GameBox
 @onready var game3d: Game3D = $"../"
 
 func get_box_color_path():
-	match box_color:
-		Shoe.ShoeType.ORANGE:
-			return "res://assets/orange/Orange"
-		Shoe.ShoeType.RED:
-			return "res://assets/red/Red"
-		Shoe.ShoeType.GREEN:
-			return "res://assets/green/Green"
-		Shoe.ShoeType.BLUE:
-			return "res://assets/blue/Blue"
-		Shoe.ShoeType.YELLOW:
-			return "res://assets/yellow/Yellow"
-		_:
-			return "Invalid " + box_color
+    match box_color:
+        Shoe.ShoeType.ORANGE:
+            return "res://assets/orange/Orange"
+        Shoe.ShoeType.RED:
+            return "res://assets/red/Red"
+        Shoe.ShoeType.GREEN:
+            return "res://assets/green/Green"
+        Shoe.ShoeType.BLUE:
+            return "res://assets/blue/Blue"
+        Shoe.ShoeType.YELLOW:
+            return "res://assets/yellow/Yellow"
+        _:
+            return "Invalid " + box_color
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# TODO: Ugly, fix later...
-	var box_size = Vector3(3, 1.25, 2)
-	var lid_size = Vector3(3.1, 0.5, 2.1)    
-	
-	# Set box collision shape
-	var box_shape = BoxShape3D.new()
-	box_shape.set_size(box_size)
-	$BoxCollisionShape.shape = box_shape
-	
-	# Set box mesh
-	var box_mesh = BoxMesh.new()
-	var box_material = StandardMaterial3D.new()
-	var box_texture = load(get_box_color_path() + "Box1.png")
-	box_material.albedo_texture = box_texture
-	box_material.uv1_scale = Vector3(3, 2, 1)
-	box_mesh.material = box_material
-	box_mesh.size = box_size
-	$BoxCollisionShape/MeshInstance3D.mesh = box_mesh
-	
-	# Set lid collision shape
-	var lid_shape = BoxShape3D.new()
-	lid_shape.set_size(box_size)
-	$LidCollisionShape.shape = lid_shape
-	
-	# Set lid mesh
-	var lid_mesh = BoxMesh.new()
-	var lid_material = StandardMaterial3D.new()
-	var lid_texture = load(get_box_color_path() + "Top1.png")
-	lid_material.albedo_texture = lid_texture
-	lid_material.uv1_scale = Vector3(3, 2, 1)
-	lid_mesh.material = lid_material
-	lid_mesh.size = lid_size
-	$LidCollisionShape/MeshInstance3D.mesh = lid_mesh
+    # TODO: Ugly, fix later...
+    var box_size = Vector3(3, 1.25, 2)
+    var lid_size = Vector3(3.1, 0.5, 2.1)    
+    
+    # Set box collision shape
+    var box_shape = BoxShape3D.new()
+    box_shape.set_size(box_size)
+    $BoxCollisionShape.shape = box_shape
+    
+    # Set box mesh
+    var box_mesh = BoxMesh.new()
+    var box_material = StandardMaterial3D.new()
+    var box_texture = load(get_box_color_path() + "Box1.png")
+    box_material.albedo_texture = box_texture
+    box_material.uv1_scale = Vector3(3, 2, 1)
+    box_mesh.material = box_material
+    box_mesh.size = box_size
+    $BoxCollisionShape/MeshInstance3D.mesh = box_mesh
+    
+    # Set lid collision shape
+    var lid_shape = BoxShape3D.new()
+    lid_shape.set_size(box_size)
+    $LidCollisionShape.shape = lid_shape
+    
+    # Set lid mesh
+    var lid_mesh = BoxMesh.new()
+    var lid_material = StandardMaterial3D.new()
+    var lid_texture = load(get_box_color_path() + "Top1.png")
+    lid_material.albedo_texture = lid_texture
+    lid_material.uv1_scale = Vector3(3, 2, 1)
+    lid_mesh.material = lid_material
+    lid_mesh.size = lid_size
+    $LidCollisionShape/MeshInstance3D.mesh = lid_mesh
 
 
 func handle_new_shoe(shoe_type: Shoe.ShoeType):
-	var score = 30
-	if box_color == shoe_type:
-		score *= 4
-	
-	game3d.engine.add_score(score)
+    var score = 30
+    if box_color == shoe_type:
+        score *= 4
+    
+    game3d.engine.add_score(score)
 
 func _on_body_entered(body):
-	if body is FlyingShoe:
-		# Save data from shoe
-		var shoe_type = body.shoe_type
-		
-		body.queue_free()
-		
-		handle_new_shoe(shoe_type)
+    if body is FlyingShoe:
+        # Save data from shoe
+        var shoe_type = body.shoe_type
+        
+        body.queue_free()
+        
+        handle_new_shoe(shoe_type)
