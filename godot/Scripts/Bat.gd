77c34b4afa760cf4d60035c1c2690df27f02df0c
Zackawa <zackarias.w@gmail.com>
2024-02-03 12:16:29 +0100

    Fixed shoes to mix between 3d

--- godot/Scripts/Bat.gd
+++ godot/Scripts/Bat.gd
@@ -13,7 +13,7 @@ func _input(event):
 	var is_left_click = event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT
 	var is_whack_down = event is InputEventKey and event.is_action_pressed("Whack")
 	if (is_left_click or is_whack_down) and currentShoe != null:
-		currentShoe.whack()
+		currentShoe.whack(1, Vector2(0,0))
 		pass
 
 func _process(_delta):
--- godot/Scripts/Shoe.gd
+++ godot/Scripts/Shoe.gd
@@ -2,13 +2,33 @@ extends RigidBody2D
 
 class_name Shoe
 
-func _ready():
-	pass # Replace with function body.
+@onready var sprite : Sprite2D = $Sprite
+
+@onready var flyingShoePacked : PackedScene = preload("res://Scenes/FlyingShoe.tscn")
 
 func _process(_delta):
 	if global_position.y > get_viewport_rect().size.y * 1.2:
 		queue_free()
+		
 
-func whack():
-	print("Whacked")
+func whack(bat_strength : float, bat_vector : Vector2):
+	# Spawn 3D shoe and hand it to 3D engine to use
+	var flying_shoe = flyingShoePacked.instantiate()
+	
+	# Calculate target from bat
+	var from = Vector2(position.x, position.y)
+	var to = from
+	
+	# Make shoe fly
+	flying_shoe.set_movement(from, to)
+	
+	# Have shoe look like this shoe
+	flying_shoe.set_looks(self)
+	
+	# Hand shoe to 3d engine for it to use
+	# engine3d.add_shoe(flying_shoe)
+	
 	queue_free()
+
+func get_textute():
+	return sprite.texture
