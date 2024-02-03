3e4a3f53ab1f7dfa4f1dd4655631c5cf5749304e
Zackawa <zackarias.w@gmail.com>
2024-02-03 10:32:43 +0100

    Menu is done and done

--- godot/Scripts/Engine.gd
+++ godot/Scripts/Engine.gd
@@ -3,7 +3,8 @@ extends Control
 class_name  GameEngine
 
 @onready var menu : MainMenu = $Menu
-@onready var game : Game = $Game
+@onready var gamePacked : PackedScene = preload("res://Scenes/Game.tscn")
+var game = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,4 +21,12 @@ func _process(delta):
 
 func start_game():
 	menu.hide()
-	game.show()
+	
+	game = gamePacked.instantiate()
+	add_child(game)
+
+func end_game():
+	game.queue_free()
+	game = null
+	
+	menu.show()
--- godot/Scripts/Game.gd
+++ godot/Scripts/Game.gd
@@ -2,14 +2,16 @@ extends Node2D
 
 class_name Game
 
+var engine : GameEngine
+
 var shoeNode = preload("res://Scenes/Shoe.tscn")
-@export var container: Node2D
+@export var container : Node2D
 @export var spawnTimeLow = 3.0
 @export var spawnTimeHigh = 6.0
 var spawnTimer = 0
 
 func _ready():
-	pass
+	engine = get_node_or_null("..")
 
 func handle_timeout():
 	var shoeInstance = shoeNode.instantiate()
@@ -26,3 +28,5 @@ func _process(delta):
 		
 	$Bat.global_position = get_global_mouse_position()
 
+func end_game():
+	engine.end_game()
