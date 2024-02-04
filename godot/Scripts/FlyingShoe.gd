4c44d080fcda8de9930c26f85bea40b46da88800
Zackawa <zackarias.w@gmail.com>
2024-02-04 11:27:29 +0100

    Boxes added and Score started

--- godot/Scripts/FlyingShoe.gd
+++ godot/Scripts/FlyingShoe.gd
@@ -17,42 +17,42 @@ var end_position : Vector3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    if flying:
-        time_flying_current += delta
-        
-        if time_flying_current >= time_flying_max:
-            time_flying_current = time_flying_max
-            flying = false
+	if flying:
+		time_flying_current += delta
+		
+		if time_flying_current >= time_flying_max:
+			time_flying_current = time_flying_max
+			flying = false
 
 func _integrate_forces(state : PhysicsDirectBodyState3D):
-    if flying:
-        #state.transform.origin = lerp(start_position, end_position, time_flying_current/time_flying_max)
-        var t = time_flying_current/time_flying_max
-        var x = start_position.x * (1 - t) + end_position.x * t
-        var z = start_position.z * (1 - t) + end_position.z * t
-        var angle_factor = 6
-        var y = -angle_factor * t * (t - 1) + end_position.y * t + start_position.y
-        state.transform.origin = Vector3(x, y, z)
+	if flying:
+		#state.transform.origin = lerp(start_position, end_position, time_flying_current/time_flying_max)
+		var t = time_flying_current/time_flying_max
+		var x = start_position.x * (1 - t) + end_position.x * t
+		var z = start_position.z * (1 - t) + end_position.z * t
+		var angle_factor = 6
+		var y = -angle_factor * t * (t - 1) + end_position.y * t + start_position.y
+		state.transform.origin = Vector3(x, y, z)
 
 func set_movement(from : Vector3, to : Vector3, duration : float):
-    flying = true
-    
-    time_flying_current = 0
-    time_flying_max = duration
-    
-    start_position = from
-    end_position = to
-    
-    position = start_position
+	flying = true
+	
+	time_flying_current = 0
+	time_flying_max = duration
+	
+	start_position = from
+	end_position = to
+	
+	position = start_position
 
 func set_type(shoe_type : Shoe.ShoeType):
-    self.shoe_type = shoe_type
+	self.shoe_type = shoe_type
 
 
 func set_looks(shoe_texture : Texture2D):
-    sprite.texture = shoe_texture
+	sprite.texture = shoe_texture
