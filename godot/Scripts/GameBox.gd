31fbe3ea62d73cedf3364dd96d397163db601cf6
Benjamin Sundvall <benjamin@sundvall.org>
2024-02-04 12:46:30 +0100

    Function for opening/closing boxes

--- godot/Scripts/GameBox.gd
+++ godot/Scripts/GameBox.gd
@@ -11,34 +11,34 @@ class_name GameBox
 @onready var game3d: Game3D
 
 
-func set_type(type: Shoe.ShoeType):
-	box_color = type
-	
-	visual_box.set_shape(box_color)
-	visual_box.set_box_open(false)
+func set_type(type: Shoe.ShoeType, is_open: bool):
+    box_color = type
+    
+    visual_box.set_shape(box_color)
+    visual_box.set_box_open(is_open)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var maybe_game3d = get_node_or_null("../")
-	if maybe_game3d is Game3D:
-		game3d = maybe_game3d 
-	
-	set_type(box_color)
+    var maybe_game3d = get_node_or_null("../")
+    if maybe_game3d is Game3D:
+        game3d = maybe_game3d 
+    
+    set_type(box_color, true)
 
 func handle_new_shoe(shoe_type: Shoe.ShoeType):
-	if game3d:
-		var score = 30
-		if box_color == shoe_type:
-			score *= 4
-		
-		game3d.game.add_score(score)
+    if game3d:
+        var score = 30
+        if box_color == shoe_type:
+            score *= 4
+        
+        game3d.game.add_score(score)
 
 
 func _on_body_entered(body):
-	if body is FlyingShoe:
-		# Save data from shoe
-		var shoe_type = body.shoe_type
-		
-		body.queue_free()
-		
-		handle_new_shoe(shoe_type)
+    if body is FlyingShoe:
+        # Save data from shoe
+        var shoe_type = body.shoe_type
+        
+        body.queue_free()
+        
+        handle_new_shoe(shoe_type)
