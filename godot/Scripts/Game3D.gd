7d99b21ebef40f31779f95e0b660485cc567111e
Benjamin Sundvall <benjamin@sundvall.org>
2024-02-04 12:51:48 +0100

    Fixed oopsie

--- godot/Scripts/Game3D.gd
+++ godot/Scripts/Game3D.gd
@@ -11,14 +11,14 @@ class_name Game3D
 
 func _ready():
 	var shoe_type_values = Shoe.ShoeType.values()
-	$Box.set_type(shoe_type_values[randi() % shoe_type_values.size()])
-	$Box2.set_type(shoe_type_values[randi() % shoe_type_values.size()])
-	$Box3.set_type(shoe_type_values[randi() % shoe_type_values.size()])
-	$Box4.set_type(shoe_type_values[randi() % shoe_type_values.size()])
-	$Box5.set_type(shoe_type_values[randi() % shoe_type_values.size()])
-	$Box6.set_type(shoe_type_values[randi() % shoe_type_values.size()])
-	$Box7.set_type(shoe_type_values[randi() % shoe_type_values.size()])
-	$Box8.set_type(shoe_type_values[randi() % shoe_type_values.size()])
+	$Box.set_type(shoe_type_values[randi() % shoe_type_values.size()], true)
+	$Box2.set_type(shoe_type_values[randi() % shoe_type_values.size()], true)
+	$Box3.set_type(shoe_type_values[randi() % shoe_type_values.size()], true)
+	$Box4.set_type(shoe_type_values[randi() % shoe_type_values.size()], true)
+	$Box5.set_type(shoe_type_values[randi() % shoe_type_values.size()], true)
+	$Box6.set_type(shoe_type_values[randi() % shoe_type_values.size()], true)
+	$Box7.set_type(shoe_type_values[randi() % shoe_type_values.size()], true)
+	$Box8.set_type(shoe_type_values[randi() % shoe_type_values.size()], true)
 
 func add_flying_shoe(flying_shoe : FlyingShoe, from : Vector2, to : Vector2, speed):
 	var from_3d = camera.project_position(from, camera.near + offset_spawn)
