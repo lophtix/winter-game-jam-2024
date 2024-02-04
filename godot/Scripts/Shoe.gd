c4711bc91cc56937740cf55dd9a35fae52a5800e
Benjamin Sundvall <benjamin@sundvall.org>
2024-02-04 12:24:01 +0100

    Shoes now keep some of their sideways velocity when hit

--- godot/Scripts/Shoe.gd
+++ godot/Scripts/Shoe.gd
@@ -11,26 +11,26 @@ enum ShoeType { BLUE, GREEN, ORANGE, RED, YELLOW }
 var game: Game
 
 func _process(_delta):
-	if global_position.y > get_viewport_rect().size.y * 1.2:
-		queue_free()
+    if global_position.y > get_viewport_rect().size.y * 1.2:
+        queue_free()
 
 func whack(bat_strength : float, _bat_vector : Vector2):
-	# Spawn 3D shoe and hand it to 3D engine to use
-	var flying_shoe : FlyingShoe = flyingShoePacked.instantiate()
-	
-	# Calculate target from bat
-	var from = Vector2(position.x, position.y)
-	var to = from
-	
-	flying_shoe.rotation.z = -rotation
-	flying_shoe.angular_velocity.z = -angular_velocity
-	
-	# Hand shoe to 3d engine for it to use
-	game.game3d.add_flying_shoe(flying_shoe, from, to, bat_strength)
-	
-	# Have shoe look like this shoe and have the same type
-	flying_shoe.set_looks(sprite.texture)
-	flying_shoe.shoe_type = shoe_type
+    # Spawn 3D shoe and hand it to 3D engine to use
+    var flying_shoe : FlyingShoe = flyingShoePacked.instantiate()
+    
+    # Calculate target from bat
+    var from = Vector2(position.x, position.y)
+    var to = from + 0.5 * Vector2(linear_velocity.x, 0)
+    
+    flying_shoe.rotation.z = -rotation
+    flying_shoe.angular_velocity.z = -angular_velocity
+    
+    # Hand shoe to 3d engine for it to use
+    game.game3d.add_flying_shoe(flying_shoe, from, to, bat_strength)
+    
+    # Have shoe look like this shoe and have the same type
+    flying_shoe.set_looks(sprite.texture)
+    flying_shoe.shoe_type = shoe_type
 
-	queue_free()
+    queue_free()
 
